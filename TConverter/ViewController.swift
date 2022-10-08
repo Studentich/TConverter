0e79a73fcfeeaaa74e33e1e4db068407ce166231
Studentich <nikita_rud2000@icloud.com>
2022-10-08 18:59:37 +0300

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Никита Рудковский on 8/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
