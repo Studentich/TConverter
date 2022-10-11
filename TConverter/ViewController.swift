fab1a98ab694f50cdf0df0135563e02f50ae7681
Studentich <nikita_rud2000@icloud.com>
2022-10-11 18:09:41 +0300

    test project two

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //pdfskoskjf;dfg
+
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet{
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+       
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        cenliusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+    }
+    
 }
+
 
